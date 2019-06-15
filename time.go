7912a632fe998603db58742196ba94206c199055
Philipp Stephani <phst@google.com>
2019-06-15 22:30:39 +0200

    Overhaul time handling.
    Use make_time and extract_time if available, provide replacements otherwise.

--- time.go
+++ time.go
@@ -16,10 +16,75 @@ package emacs
 
 import (
 	"fmt"
+	"math"
 	"math/big"
 	"reflect"
 	"time"
 )
+
+// #include <assert.h>
+// #include <inttypes.h>
+// #include <limits.h>
+// #include <stddef.h>
+// #include <stdint.h>
+// #include <stdlib.h>
+// #include <time.h>
+// #include <emacs-module.h>
+// static_assert(PTRDIFF_MAX <= SIZE_MAX, "unsupported architecture");
+// static_assert((time_t)1.5 == 1, "unsupported architecture");
+// static_assert(LONG_MAX >= 1000000000, "unsupported architecture");
+// struct timespec extract_time(emacs_env *env, emacs_value value) {
+// #if defined EMACS_MAJOR_VERSION && EMACS_MAJOR_VERSION >= 27
+//   if ((size_t)env->size > offsetof(emacs_env, extract_time)) {
+//     return env->extract_time(env, value);
+//   }
+// #endif
+//   emacs_value list = env->funcall(env, env->intern(env, "seconds-to-time"), 1, &value);
+//   emacs_value car = env->intern(env, "car");
+//   emacs_value cdr = env->intern(env, "cdr");
+//   intmax_t parts[4] = {0};
+//   for (int i = 0; i < 4; ++i) {
+//     parts[i] = env->extract_integer(env, env->funcall(env, car, 1, &list));
+//     list = env->funcall(env, cdr, 1, &list);
+//     if (!env->is_not_nil(env, list)) break;
+//   }
+//   struct timespec result;
+//   assert(parts[1] >= 0 && parts[1] <= 0x10000);
+//   if (__builtin_mul_overflow(parts[0], 0x10000, &result.tv_sec) ||
+//       __builtin_add_overflow(result.tv_sec, parts[1], &result.tv_sec)) {
+//     env->non_local_exit_signal(env, env->intern(env, "overflow-error"), env->intern(env, "nil"));
+//     return result;
+//   }
+//   assert(parts[2] >= 0 && parts[2] < 1000000);
+//   assert(parts[3] >= 0 && parts[3] < 1000000);
+//   result.tv_nsec = (long) parts[2] * 1000 + (long) parts[3] / 1000;
+//   return result;
+// }
+// emacs_value make_time(emacs_env *env, struct timespec time) {
+// assert(time.tv_nsec >= 0 && time.tv_nsec < 1000000000);
+// #if defined EMACS_MAJOR_VERSION && EMACS_MAJOR_VERSION >= 27
+//   if ((size_t)env->size > offsetof(emacs_env, make_time)) {
+//     return env->make_time(env, time);
+//   }
+// #endif
+//   imaxdiv_t seconds = imaxdiv(time.tv_sec, 0x10000);
+//   if (seconds.rem < 0) {
+//     --seconds.quot;
+//     seconds.rem += 0x10000;
+//   }
+//   assert(seconds.rem >= 0 && seconds.rem <= 0x10000);
+//   ldiv_t nanos = ldiv(time.tv_nsec, 1000);
+//   assert(nanos.quot >= 0 && nanos.quot < 1000000);
+//   assert(nanos.rem >= 0 && nanos.rem < 1000000);
+//   emacs_value args[] = {
+//     env->make_integer(env, seconds.quot),
+//     env->make_integer(env, seconds.rem),
+//     env->make_integer(env, nanos.quot),
+//     env->make_integer(env, nanos.rem * 1000)
+//   };
+//   return env->funcall(env, env->intern(env, "list"), 4, args);
+// }
+import "C"
 
 // Time is a type with underlying type time.Time that knows how to convert
 // itself from and to an Emacs time value.
@@ -28,31 +93,37 @@ type Time time.Time
 // String formats the time as a string.  It calls time.Time.String.
 func (t Time) String() string { return time.Time(t).String() }
 
-// Emacs returns a quadruple (high low μs ps) in the same format as the Emacs
-// function current-time.
+// Emacs returns an Emacs timestamp as a pair (ticks . hz) or a quadruple
+// (high low μs ps) in the same format as the Emacs function current-time.
 func (t Time) Emacs(e Env) (Value, error) {
-	var ps Picoseconds
-	ps.FromTime(time.Time(t))
-	return ps.Emacs(e)
+	x := time.Time(t)
+	return e.makeTime(x.Unix(), x.Nanosecond())
 }
 
-// FromEmacs sets *t to the Go equivalent of the Emacs time information in v.
-// v can have the same formats as for the function format‑time‑string: a number
-// of seconds since the epoch, a pair (high low) or (high . low), a triple
-// (high low μs), or a quadruple (high low μs ps).  The picosecond value is
-// truncated to nanosecond precision.  If the Emacs time value would overflow a
-// Go time, FromEmacs returns an error.
+// FromEmacs sets *t to the Go equivalent of the Emacs time value in v.  v can
+// be any time value: nil (for the current time), a number of seconds since the
+// epoch, a pair (ticks . hz), a pair (high low), a triple (high low μs), or a
+// quadruple (high low μs ps).  The picosecond value is truncated to nanosecond
+// precision.  If the Emacs time value would overflow a Go time, FromEmacs
+// returns an error.
 func (t *Time) FromEmacs(e Env, v Value) error {
-	var ps Picoseconds
-	if err := ps.FromEmacs(e, v); err != nil {
-		return err
-	}
-	r, err := ps.Time()
+	r, err := e.Time(v)
 	if err != nil {
 		return err
 	}
 	*t = Time(r)
 	return nil
+}
+
+// Time returns the Go equivalent of the Emacs time value in v.  v can be any
+// time value: nil (for the current time), a number of seconds since the epoch,
+// a pair (ticks . hz), a pair (high low), a triple (high low μs), or a
+// quadruple (high low μs ps).  The picosecond value is truncated to nanosecond
+// precision.  If the Emacs time value would overflow a Go time, Time returns
+// an error.
+func (e Env) Time(v Value) (time.Time, error) {
+	s, ns, err := e.extractTime(v)
+	return time.Unix(s, int64(ns)), err
 }
 
 // Duration is a type with underlying type time.Duration that knows how to
@@ -62,31 +133,48 @@ type Duration time.Duration
 // String formats the duration as a string.  It calls time.Duration.String.
 func (d Duration) String() string { return time.Duration(d).String() }
 
-// Emacs returns a quadruple (high low μs ps) in the same format as the Emacs
-// function current-time.
+// Emacs returns an Emacs timestamp as a pair (ticks . hz) or a quadruple
+// (high low μs ps) in the same format as the Emacs function current-time.
 func (d Duration) Emacs(e Env) (Value, error) {
-	var ps Picoseconds
-	ps.FromDuration(time.Duration(d))
-	return ps.Emacs(e)
+	x := time.Duration(d)
+	s, ns := int64(x/time.Second), int(x%time.Second)
+	if ns < 0 {
+		s--
+		ns += int(time.Second)
+	}
+	return e.makeTime(s, ns)
 }
 
-// FromEmacs sets *d to the Go equivalent of the Emacs time information in v.
-// v can have the same formats as for the function format‑time‑string: a number
-// of seconds, a pair (high low) or (high . low), a triple (high low μs), or a
-// quadruple (high low μs ps).  The picosecond value is truncated to nanosecond
-// precision.  If the Emacs time value would overflow a Go duration, FromEmacs
-// returns an error.
+// FromEmacs sets *d to the Go equivalent of the Emacs time value in v,
+// interpreted as a duration.  v can be any time value: nil (for the current
+// time), a number of seconds, a pair (ticks . hz), a pair (high low), a triple
+// (high low μs), or a quadruple (high low μs ps).  The picosecond value is
+// truncated to nanosecond precision.  If the Emacs time value would overflow a
+// Go duration, FromEmacs returns an error.
 func (d *Duration) FromEmacs(e Env, v Value) error {
-	var ps Picoseconds
-	if err := ps.FromEmacs(e, v); err != nil {
-		return err
-	}
-	r, err := ps.Duration()
+	r, err := e.Duration(v)
 	if err != nil {
 		return err
 	}
 	*d = Duration(r)
 	return nil
+}
+
+// Duration returns the Go equivalent of the Emacs time value in v, interpreted
+// as a duration.  v can be any time value: nil (for the current time), a
+// number of seconds, a pair (ticks . hz) a pair (high low), a triple
+// (high low μs), or a quadruple (high low μs ps).  The picosecond value is
+// truncated to nanosecond precision.  If the Emacs time value would overflow a
+// Go duration, Duration returns an error.
+func (e Env) Duration(v Value) (time.Duration, error) {
+	s, ns, err := e.extractTime(v)
+	if err != nil {
+		return 0, err
+	}
+	if s <= math.MinInt64/int64(time.Second) || s >= math.MaxInt64/int64(time.Second) {
+		return 0, OverflowError(fmt.Sprintf("%d.%09ds", s, ns))
+	}
+	return time.Duration(s)*time.Second + time.Duration(ns), nil
 }
 
 // Picoseconds represents a number of picoseconds.
@@ -246,6 +334,15 @@ var (
 	billion  = big.NewInt(1e9)
 	lowMask  = big.NewInt(0xFFFF)
 )
+
+func (e Env) makeTime(s int64, ns int) (Value, error) {
+	return e.checkRaw(C.make_time(e.raw(), C.struct_timespec{C.time_t(s), C.long(ns)}))
+}
+
+func (e Env) extractTime(v Value) (s int64, ns int, err error) {
+	t := C.extract_time(e.raw(), v.r)
+	return int64(t.tv_sec), int(t.tv_nsec), e.check()
+}
 
 func timeIn(v reflect.Value) In   { return Time(v.Interface().(time.Time)) }
 func timeOut(v reflect.Value) Out { return (*Time)(v.Interface().(*time.Time)) }
--- time_test.go
+++ time_test.go
@@ -15,7 +15,11 @@
 package emacs
 
 import (
+	"fmt"
+	"log"
 	"math/big"
+	"math/rand"
+	"reflect"
 	"testing"
 	"testing/quick"
 	"time"
@@ -80,4 +84,182 @@ func TestQuad(t *testing.T) {
 	if err := quick.Check(f, nil); err != nil {
 		t.Error(err)
 	}
+}
+
+func init() {
+	ERTTest(goTimeRoundtrip)
+	ERTTest(goDurationRoundtrip)
+	ERTTest(emacsTimeRoundtrip)
+	ERTTest(emacsDurationRoundtrip)
+}
+
+func goTimeRoundtrip(e Env) error {
+	f := func(a Time) bool {
+		v, err := a.Emacs(e)
+		if err != nil {
+			log.Printf("couldn’t convert time %s to Emacs: %v", a, e.Message(err))
+			return false
+		}
+		b, err := e.Time(v)
+		if err != nil {
+			log.Printf("couldn’t convert time from Emacs: %v", e.Message(err))
+			return false
+		}
+		return time.Time(a).Equal(b)
+	}
+	return quick.Check(f, nil)
+}
+
+func (Time) Generate(rand *rand.Rand, size int) reflect.Value {
+	return reflect.ValueOf(Time(time.Unix(rand.Int63(), rand.Int63())))
+}
+
+func goDurationRoundtrip(e Env) error {
+	f := func(a Duration) bool {
+		v, err := a.Emacs(e)
+		if err != nil {
+			log.Printf("couldn’t convert duration %s to Emacs: %v", a, e.Message(err))
+			return false
+		}
+		b, err := e.Duration(v)
+		if err != nil {
+			log.Printf("couldn’t convert duration from Emacs: %v", e.Message(err))
+			return false
+		}
+		return time.Duration(a) == b
+	}
+	return quick.Check(f, nil)
+}
+
+func emacsTimeRoundtrip(e Env) error {
+	f := func(q quad) bool {
+		a, trunc, ok := q.lists(e)
+		if !ok {
+			return false
+		}
+		t, err := e.Time(a)
+		if e.IsOverflowError(err) {
+			// Treat overflows as success.
+			return true
+		}
+		if err != nil {
+			log.Printf("couldn’t convert time from Emacs: %v", e.Message(err))
+			return false
+		}
+		b, err := Time(t).Emacs(e)
+		if err != nil {
+			log.Printf("couldn’t convert time %s to Emacs: %v", t, e.Message(err))
+			return false
+		}
+		// We need to compare the truncated value.
+		return equalTime(e, trunc, b)
+	}
+	return quick.Check(f, &quick.Config{MaxCountScale: 10})
+}
+
+func emacsDurationRoundtrip(e Env) error {
+	f := func(q quad) bool {
+		a, trunc, ok := q.lists(e)
+		if !ok {
+			return false
+		}
+		d, err := e.Duration(a)
+		if e.IsOverflowError(err) {
+			// Treat overflows as success.
+			return true
+		}
+		if err != nil {
+			log.Printf("couldn’t convert duration from Emacs: %v", e.Message(err))
+			return false
+		}
+		b, err := Duration(d).Emacs(e)
+		if err != nil {
+			log.Printf("couldn’t convert duration %s to Emacs: %v", d, e.Message(err))
+			return false
+		}
+		// We need to compare the truncated value.
+		return equalTime(e, trunc, b)
+	}
+	return quick.Check(f, &quick.Config{MaxCountScale: 10})
+}
+
+type quad struct {
+	hi         int64
+	lo, μs, ps uint16
+}
+
+func (quad) Generate(rand *rand.Rand, size int) reflect.Value {
+	return reflect.ValueOf(quad{
+		// We intentionally leave the upper 16 bit out.  On typical
+		// systems, time_t is a 64-bit signed integer, so filling all
+		// the bits would cause almost 100% overflow errors and no real
+		// tests.  We cast to signed first to have some negative
+		// numbers.
+		int64(rand.Uint64()) >> 16,
+		// The other parts have technically a smaller domain than
+		// uint16, but the functions should accept denormalized values
+		// as well.
+		uint16(rand.Uint32()), uint16(rand.Uint32()), uint16(rand.Uint32()),
+	})
+}
+
+func (q quad) String() string {
+	return fmt.Sprintf("(%d %d %d %d)", q.hi, q.lo, q.μs, q.ps)
+}
+
+func (q quad) lists(e Env) (precise, truncated Value, ok bool) {
+	precise, ok = q.list(e)
+	if !ok {
+		return
+	}
+	q.ps -= q.ps % 1000
+	truncated, ok = q.list(e)
+	return
+}
+
+func (q quad) list(e Env) (v Value, ok bool) {
+	v, err := e.List(Int(q.hi), Int(q.lo), Int(q.μs), Int(q.ps))
+	if err != nil {
+		log.Printf("couldn’t convert time quadruple %s to list: %v", q, e.Message(err))
+	}
+	return v, err == nil
+}
+
+// equalTime returns whether the two Emacs time values in a and b are equal.
+func equalTime(e Env, a, b Value) bool {
+	// Rewrite: a = b ⟺ a − b = 0 ⟺ ¬(a − b < 0 ∨ 0 < a − b)
+	d, err := e.Call("time-subtract", a, b)
+	if err != nil {
+		log.Printf("couldn’t subtract time values: %s", e.Message(err))
+		return false
+	}
+	// Emacs doesn’t have a time-equal-p function or similar.
+	pos, err := timeLess(e, Int(0), d)
+	if err != nil {
+		// Don’t bother returning the error since the test functions
+		// above treat different times as an error.
+		log.Printf("couldn’t compare times: %s", e.Message(err))
+		return false
+	}
+	neg, err := timeLess(e, d, Int(0))
+	if err != nil {
+		// Don’t bother returning the error since the test functions
+		// above treat different times as an error.
+		log.Printf("couldn’t compare times: %s", e.Message(err))
+		return false
+	}
+	equal := !(pos || neg)
+	if !equal {
+		log.Print(e.FormatMessage("time values %s and %s aren’t equal, difference is %s", a, b, d))
+		// Don’t bother with returning the error since the test
+		// functions above treat different times as an error.
+	}
+	return equal
+}
+
+// timeLess returns whether a < b for two Emacs time values.
+func timeLess(e Env, a, b In) (bool, error) {
+	var less Bool
+	err := e.CallOut("time-less-p", &less, a, b)
+	return bool(less), err
 }
